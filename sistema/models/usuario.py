777813f96fb232b3d66a0860e2c291d222608a9e
leandro291 <lenadrito1@gmail.com>
2026-06-02 00:26:53 -0500

    implementacion de cuenta en clase usuario

--- sistema/models/usuario.py
+++ sistema/models/usuario.py
@@ -1,23 +1,28 @@
 from typing import Dict
-from pydantic import BaseModel, Field, field_validator, ValidationError
+from cuenta import Cuenta
 from utils.utils import limpiar_strings
+from pydantic import BaseModel, Field, field_validator, ValidationError
 
 class Usuario:
     def __init__(self, nombre: str, dni: str):
         self.nombre = nombre
         self.dni = dni
-        self.cuentas = {}
-        self.estado = True
+        self.cuentas: Dict[str, Cuenta] = {}
+        self.estado: bool = True
 
-    def _cambiar_estado(self):
-        pass
+    def _cambiar_estado(self) -> None:
+        
+        self.estado = not self.estado
 
-    def _validar_estado(self):
-        pass
+    def _validar_estado(self) -> None:
+        
+        if not self.estado:
+            raise ValueError("El usuario se encuentra inactivo")
 
-    def crear_cuenta(self):
-        pass
-
+    def crear_cuenta(self, cuenta: Cuenta) -> None:
+        
+        self._validar_estado()
+        self.cuentas[cuenta.numero_cuenta] = cuenta
 
 class UsuarioSchema(BaseModel):
     nombre: str = Field(min_length=1, max_length=60)
@@ -49,6 +54,17 @@ class UsuarioSchema(BaseModel):
         return valor_limpio
 
 def main():
+
+    try:
+        validador_usuario = UsuarioSchema(nombre="Leandro", dni="60746986")
+        usuario1 = Usuario(nombre=validador_usuario.nombre, dni=validador_usuario.dni)
+
+        print(usuario1.nombre)
+        print(usuario1.dni)
+
+    except ValidationError as e:
+        print(f"Error: {e}")
+
     pass
 
 if __name__ == "__main__":
