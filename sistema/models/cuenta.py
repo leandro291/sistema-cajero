20a87dd6befc90c5f2713d2692e2035a3cbf310b
leandro291 <lenadrito1@gmail.com>
2026-06-02 15:41:29 -0500

    agregar métodos para validar estado y monto en la clase Cuenta

--- sistema/models/cuenta.py
+++ sistema/models/cuenta.py
@@ -7,6 +7,32 @@ class Cuenta:
         self.saldo = 0
         self.estado = True
 
+    def _cambiar_estado(self) -> None:
+        self.estado = not self.estado
+    
+    def _validar_estado(self) -> None:
+        if not self.estado:
+            raise ValueError("La cuenta se encuentra inactiva")
+        
+    def _validar_monto(self, monto: float) -> None:
+        
+        if monto <= 0:
+            raise ValueError("El monto depositado no puede ser menor a 0")
+    
+    def depositar(self, monto: float) -> None:
+
+        self._validar_estado()
+        self._validar_monto()
+        
+        self.saldo += monto
+
+    def retirar(self, monto: float) -> None:
+
+        self._validar_estado()
+        self._validar_monto()
+
+        self.saldo -= monto
+
 class CuentaSchema(BaseModel):
     
     numero_cuenta: str
