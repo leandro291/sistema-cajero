19c2d582d3fbe806ce53539c7088c44bed498ba4
leandro291 <lenadrito1@gmail.com>
2026-06-03 19:02:47 -0500

    fix: corregir asignación de dinero_disponible en el constructor de Cajero y mejorar la validación en retirar_dinero

--- sistema/models/cajero.py
+++ sistema/models/cajero.py
@@ -1,9 +1,34 @@
+from .tarjeta import Tarjeta
+from .cuenta import Cuenta
+from utils import limpiar_strings
 from pydantic import BaseModel, field_validator
 
 class Cajero:
     
     def __init__(self, dinero_disponible: float):
-        self.dinero_disponible = float
+        self.dinero_disponible = dinero_disponible
+
+    def retirar_dinero(self, cuenta: "Cuenta", tarjeta: "Tarjeta", pin: str, monto: float) -> None:
+        pin_limpio = limpiar_strings(pin)
+
+        if not pin_limpio.isdigit():
+            raise ValueError("El PIN solo debe contener valores numéricos.")
+        
+        if not isinstance(monto, float):
+            raise TypeError("El monto ingresado no es válido.")
+        
+        if monto <= 0:
+            raise ValueError("El monto debe ser mayor a 0.")
+
+        if tarjeta.validar_pin(pin_ingresado=pin_limpio):
+            cuenta.debitar(monto=monto)
+            self.dinero_disponible -= monto
+        else:
+            raise ValueError(f"PIN incorrecto. Le quedan {3 - tarjeta.intentos} intentos.")
+    
+    def __str__(self):
+        return str({"dinero_disponible" : self.dinero_disponible})
+
 
 class CajeroSchema(BaseModel):
 
