404fbb08ae81f5d84ca03df57bb26d9fedf2fcec
leandro291 <lenadrito1@gmail.com>
2026-06-04 00:33:59 -0500

    refactor: mejorar validaciones y estructura en la clase Cajero y CajeroSchema

--- sistema/models/cajero.py
+++ sistema/models/cajero.py
@@ -1,7 +1,7 @@
 from .tarjeta import Tarjeta
 from .cuenta import Cuenta
 from utils import limpiar_strings
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, Field
 
 class Cajero:
     
@@ -13,12 +13,15 @@ class Cajero:
 
         if not pin_limpio.isdigit():
             raise ValueError("El PIN solo debe contener valores numéricos.")
-        
-        if not isinstance(monto, float):
+        if not isinstance(monto, (int, float)):
             raise TypeError("El monto ingresado no es válido.")
-        
         if monto <= 0:
             raise ValueError("El monto debe ser mayor a 0.")
+        
+        if monto > self.dinero_disponible:
+            raise ValueError("El cajero no cuenta con el efectivo suficiente para esta transacción.")
+        if tarjeta.numero_tarjeta not in cuenta.tarjetas:
+            raise ValueError("Operación denegada: La tarjeta no está vinculada a esta cuenta.")
 
         if tarjeta.validar_pin(pin_ingresado=pin_limpio):
             cuenta.debitar(monto=monto)
@@ -27,20 +30,10 @@ class Cajero:
             raise ValueError(f"PIN incorrecto. Le quedan {3 - tarjeta.intentos} intentos.")
     
     def __str__(self):
-        return str({"dinero_disponible" : self.dinero_disponible})
+        return f"Cajero(efectivo_disponible={self.dinero_disponible})"
 
 
 class CajeroSchema(BaseModel):
 
-    dinero_disponible : float
+    dinero_disponible : float = Field(gt=0.0)
 
-    @field_validator('dinero_disponible')
-    @classmethod
-    def validation_dinero_disponible(cls, valor: float):
-
-        if not isinstance(valor, (float)):
-            raise TypeError("El monto ingresado no es valido")
-        
-        if valor <= 0:
-            raise ValueError("El monto no puede ser menor igual a 0")
-    
