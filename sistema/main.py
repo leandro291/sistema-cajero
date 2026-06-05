e13753c076b7642ec9f8375671333cc37bb644cc
leandro291 <lenadrito1@gmail.com>
2026-06-05 13:49:27 -0500

    feat: simplificar el menú del SistemaCajero y mejorar la gestión de opciones

--- sistema/main.py
+++ sistema/main.py
@@ -1,93 +1,51 @@
 from services import SistemaCajero
-from pydantic import ValidationError
 
 def main():
-
+    
+    sistema = SistemaCajero()
     while True:
         try:
-            sistema = SistemaCajero()
+            print("\n" + "="*45)
+            print("SISTEMA BANCARIO Y CAJERO ATM")
+            print("="*45)
             
-            print("\n---- Sistema de Cajero (ATM) ----")
-            print("[1]. Administracion")
-            print("[2]. Movimiento virtual (Usuario <-> Cuenta) ")
-            print("[3]. Movimiento Fisico (ATM)")
-            print("[4]. Salir")
+            print("\n--- AGENCIA BANCARIA  ---")
+            print("[1]. Registrar nuevo cliente")
+            print("[2]. Aperturar cuenta bancaria")
+            print("[3]. Emitir tarjeta de débito")
+            print("[4]. Depositar efectivo a cuenta") 
+            
+            print("\n--- OPERACIONES EN EL CAJERO ---")
+            print("[5]. Consultar saldo disponible") 
+            print("[6]. Retirar dinero efectivo") 
+            print("[7]. Salir del sistema")
 
-            opc = int(input("Ingrese una opcion: "))
+            opc = int(input("Ingrese una opción: "))
                 
-            match(opc):
+            match opc:
                 case 1:
-
-                    while True:
-                        print("\n ---- Administracion ----")
-                        print("[1]. Registrar nuevo usuario")
-                        print("[2]. Vincular cuenta bancaria")
-                        print("[3]. Emitir tarjeta de débito")
-                        print("[4]. Salir")
-
-                        opc = int(input("Ingrese una opcion: "))
-                        
-                        match(opc):
-                            case 1:
-                                sistema.crear_usuario()
-                            case 2:
-                                sistema.crear_y_vincular_cuenta()
-                            case 3:
-                                sistema.crear_y_vincular_tarjeta()
-                            case 4:
-                                break
-                            case _:
-                                print("Opcion invalida. Vuelva a seleccionar una opcion")
-
+                    sistema.crear_usuario()
                 case 2:
-
-                    while True:
-                        print("\n ---- Movimientos virtuales ----")
-                        print("[1]. Transferencia entrante (Ingresar dinero)")
-                        print("[2]. Transferencia saliente (Retirar a billetera)")
-                        print("[3]. Salir")
-
-                        opc = int(input("Ingrese una opcion: "))
-
-                        match(opc):
-                            case 1:
-                                pass
-                            case 2:
-                                pass
-                            case 3:
-                                pass
-                            case _:
-                                pass
-
+                    sistema.crear_y_vincular_cuenta()
                 case 3:
-                    while True:
-                        print("\n ---- Movimiento Fisico ----")
-                        print("[1]. Usar Cajero Automático")
-                        print("[2]. Salir")
-
-                        opc = int(input("Ingrese una opcion: "))
-
-                        match(opc):
-                            case 1:
-                                pass
-                            case 2:
-                                pass
-                            case 3:
-                                pass
-                            case _:
-                                pass
-                    
+                    sistema.crear_y_vincular_tarjeta()
                 case 4:
-                    print(f"Saliendo del sistema...")
-                    break
-
+                    sistema.depositar_fondos_cuenta()
+                case 5:
+                    pass
+                case 6:
+                    sistema.retirar_dinero_cajero()
+                case 7:
+                    print("Saliendo del sistema...")
+                    break  
                 case _:
-                    print("Opcion invalida. Vuelva a seleccionar una opcion")
-        except KeyboardInterrupt as e:
-            print(f"Error: {e}")
-        except Exception as e:
-            print(f"Error: {e}")
+                    print("pción inválida. Vuelva a seleccionar una opción.")
 
+        except KeyboardInterrupt:
+            print("\nPrograma interrumpido por el usuario (Ctrl+C). Saliendo...")
+            break
+        except Exception as e:
+            print(f"\nError crítico inesperado: {e}")
 
 if __name__ == "__main__":
     main()
