e98aa17f5ded97f2f0d4377a59f4f4016770ebb4
leandro291 <lenadrito1@gmail.com>
2026-06-04 20:07:34 -0500

    feat: agregar funciones para crear y vincular tarjetas en SistemaCajero

--- sistema/services/sistema_cajero.py
+++ sistema/services/sistema_cajero.py
@@ -1,10 +1,12 @@
-from models import Cuenta, CuentaSchema, Usuario, UsuarioSchema
+from models import Cuenta, CuentaSchema, Usuario, UsuarioSchema, Tarjeta, TarjetaSchema, Cajero, CajeroSchema
 from typing import Dict
 
 class SistemaCajero:
     def __init__(self):
         self.usuarios: Dict[str, "Usuario"]  = {}
         self.cuentas: Dict[str, "Cuenta"] = {}
+        self.tarjetas: Dict[str, "Tarjeta"] = {}
+        self.cajeros: Dict[str, "Cajero"] = {}
 
     def _lista_de_usuarios(self) -> None:
         print("=== LISTA DE USUARIOS ===")
@@ -36,9 +38,11 @@ class SistemaCajero:
         print(f"======= VINCULACION DE CUENTAS =======")
         self._lista_de_usuarios()
 
-        dni_usuario = input("Ingrese DNI del usuario al que desea vincular").strip()
+        dni_usuario = input("Ingrese DNI del usuario al que desea vincular: ").strip()
         if dni_usuario not in self.usuarios:
             raise ValueError("El DNI ingresado no se encuentra registrado")
+        
+        print(f"======= CREACION DE CUENTA =======")
 
         numero_cuenta = input("Ingrese el numero de cuenta: ").strip()
         if numero_cuenta in self.cuentas:
@@ -62,6 +66,43 @@ class SistemaCajero:
         usuario: "Usuario" = self.usuarios[dni_usuario]
         usuario.vincular_cuenta(cuenta.numero_cuenta)
         print(f"La cuenta {cuenta.numero_cuenta} ha sido vinculada correctamente hacia {usuario.nombre}")
+
+    def crear_y_vincular_tarjeta(self):
+        print(f"======= VINCULACION DE TARJETAS =======")
+        self._lista_de_usuarios()
+
+        dni_usuario = input("Ingrese DNI del usuario al que desea vincular: ").strip()
+        if dni_usuario not in self.usuarios:
+            raise ValueError("El DNI ingresado no se encuentra registrado")
+        
+        usuario: "Usuario" = self.usuarios[dni_usuario]
+        print("======= CUENTAS DISPONIBLES =======")
+        for cuenta in usuario.cuentas:
+            print(f"- {cuenta}")
+
+        numero_cuenta = input("Ingrese el numero de cuenta: ").strip()
+        if numero_cuenta not in self.cuentas:
+            raise ValueError("La cuenta ingresada no se encuentra registrado")
+        
+        print(f"======= CREACION DE TARJETA =======")
+
+        pin = input("Ingrese el PIN para su tarjeta: ").strip()
+        
+        validacion_tarjeta = TarjetaSchema(
+            pin=pin,
+            numero_cuenta=numero_cuenta
+        )
+
+        tarjeta = Tarjeta(
+            pin=validacion_tarjeta.pin,
+            numero_cuenta= validacion_tarjeta.numero_cuenta
+        )
+
+        self.tarjetas[tarjeta.numero_tarjeta] = tarjeta
+        cuenta : "Cuenta" = self.cuentas[numero_cuenta]
+        cuenta.vincular_tarjeta(tarjeta.numero_tarjeta)
+        print(f"La tarjeta {tarjeta.numero_tarjeta} ha sido vinculada exitosamente a la cuenta {cuenta.numero_cuenta}")
+
 
 def main():
     sistema = SistemaCajero()
