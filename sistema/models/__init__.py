4239775faef0e021cc797e5708f0761bb8f5ab73
leandro291 <lenadrito1@gmail.com>
2026-06-06 10:06:46 -0500

    feat: agregar importación de la clase Transaccion en el módulo de modelos

--- sistema/models/__init__.py
+++ sistema/models/__init__.py
@@ -2,3 +2,4 @@ from .cuenta import Cuenta, CuentaSchema
 from .usuario import Usuario, UsuarioSchema
 from .tarjeta import Tarjeta, TarjetaSchema
 from .cajero import Cajero, CajeroSchema
+from .transaccion import Transaccion
