7ede07fc506ce7f0c2dfafeb3724fbed1c3bf982
leandro291 <lenadrito1@gmail.com>
2026-06-06 11:05:04 -0500

    feat: corregir inicialización de atributos en la clase Transaccion y agregar TransaccionSchema

--- sistema/models/transaccion.py
+++ sistema/models/transaccion.py
@@ -1,10 +1,20 @@
 from datetime import now
+from pydantic import BaseModel
 
 class Transaccion:
-    def __init__(self, numero_transaccion: str, numero_cajero: str, numero_cuenta: str, monto: float, tipo: str, ):
-        self.numero_transaccion = numero_transaccion
-        self.numero_cajero = numero_cajero
-        self.numero_cuenta = self.numero_cuenta
+    def __init__(self, numero_transaccion: str, numero_cajero: str, numero_cuenta: str, monto: float, tipo: str):
+        self.numero_transaccion = numero_transaccion 
+        self.numero_cajero = numero_cajero 
+        self.numero_cuenta = numero_cuenta 
         self.monto = monto
         self.tipo = tipo
         self.fecha = now().strftime("%Y-%m-%d %H:%M:%S")
+    
+class TransaccionSchema(BaseModel):
+
+    numero_transaccion: str
+    numero_cajero: str
+    numero_cuenta: str
+    monto: float
+    tipo: str
+
