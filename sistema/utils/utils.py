de5eddc07445875c69466622807be7ad91a57202
leandro291 <lenadrito1@gmail.com>
2026-06-08 12:04:19 -0500

    feat: agregar función validar_luhn para verificar la validez de números

--- sistema/utils/utils.py
+++ sistema/utils/utils.py
@@ -9,3 +9,25 @@ def limpiar_strings(texto: str) -> str:
         raise ValueError("El texto no se debe encontrar vacio")
     
     return texto_limpio
+
+def validar_luhn(numero):
+    digitos = str(numero).replace(" ", "")
+    
+    if not digitos.isdigit():
+        return False
+        
+    suma = 0
+    es_segundo = False
+    
+    for digito in reversed(digitos):
+        valor = int(digito)
+        
+        if es_segundo:
+            valor *= 2
+            if valor > 9:
+                valor = (valor % 10) + 1
+                
+        suma += valor
+        es_segundo = not es_segundo
+        
+    return suma % 10 == 0
